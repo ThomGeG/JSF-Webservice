e31367dd26de6ac5d779372fe4ca0120162576d7
thomasgaskell <thomasgaskell@SANDY610.sandstone.local>
2018-09-07 09:29:23 +1000

    Aired out a smell (Utilized the existing helper method)

--- src/main/java/authorization/UserServiceImpl.java
+++ src/main/java/authorization/UserServiceImpl.java
@@ -13,11 +13,11 @@ public class UserServiceImpl implements UserService {
 	private final Map<String, User> users = new ConcurrentHashMap<>();
 	
 	public UserServiceImpl() {
-		users.put("tom", new User("Tom", "password")); //TODO - Delete this dummy user
+		saveUser(new User("Tom", "password")); //TODO - Delete this dummy user
 	}
 	
 	@Override
-	public User getUser(String username) {
+	public User getUser(String username) {		
 		return users.get(username.toLowerCase());
 	}
 	
